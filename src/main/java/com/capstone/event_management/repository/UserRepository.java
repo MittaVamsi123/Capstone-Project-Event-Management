0ff39ddfecfb371627ee5a77cfe390857005d14e
MittaVamsi123 <127467992+MittaVamsi123@users.noreply.github.com>
2023-09-27 23:10:48 +0530

    Delete src directory

--- src/main/java/com/capstone/event_management/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.event_management.repository;
-
-import com.capstone.event_management.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
